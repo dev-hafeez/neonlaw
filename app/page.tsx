b975356a5e9812f84a4b0a541e2c06a562b5fa50
dev-hafeez <159642563+dev-hafeez@users.noreply.github.com>
2025-08-06 18:49:54 +0500

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/navbar"
 import PinterestMasonry from "@/components/pinterest-masonry"
+import HeroTransition from "@/components/HeroTransition"
 
 export default function Page() {
   return (
 <div className="bg-[#f9f9f9] relative">
+          <HeroTransition />
       {/* Background NEON Logo */}
-      <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-0">
-        <img src="/neon-logo.png" alt="NEON Background Logo" className="w-96 h-96 object-contain" />
+      <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-0 ">
+        <img src="/Blue.png" alt="NEON Background Logo" className="w-96 h-96 object-contain" />
       </div>
 
       {/* Cards start from top - behind navbar */}
