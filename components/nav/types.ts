21c278278df7008a8a59b6b3fcb8abcf0c835ffe
dev-hafeez <159642563+dev-hafeez@users.noreply.github.com>
2025-09-09 17:50:48 +0500

    Delete components directory

--- components/nav/types.ts
+++
@@ -1,8 +0,0 @@
-export interface NavLink { title: string; href: string; showAll?: boolean }
-
-export interface DropdownProps {
-  title: string;
-  links: NavLink[];
-  isOpen: boolean;
-  onOpenChange: (isOpen: boolean) => void;
-}
